cb81c7355dd1abb6437110fbf620ff01a1cb7101
PubTrades <chrisb@publictrades.com>
2024-07-11 00:41:50 -0400

    done with all updates

--- app/api/sendMessage/route.js
+++ app/api/sendMessage/route.js
@@ -1,12 +1,79 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+import { GoogleAIFileManager } from "@google/generative-ai/server";
 
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { message } = req.body;
-        // Process the message (e.g., save to database, send email, etc.)
-        console.log("Received message:", message);
-        res.status(200).json({ success: true });
-    } else {
-        res.status(405).end(); // Method Not Allowed
+// Ensure environment variables are loaded
+const apiKey = process.env.GEMINI_API_KEY;
+
+export async function POST(req, res) {
+    try {
+
+        let aboutMe = "##  Bringing Ideas to Life Through Code\n\nI'm Chris, a New York City-based full-stack software developer with a passion for transforming innovative ideas into tangible digital experiences. My journey in the tech world began long before my immersive dive into software engineering at Purdue University.  I honed my skills and collaborated on impactful projects at renowned companies like Bloomberg LP and BBDO Worldwide, gaining valuable experience in understanding user needs and building solutions that make a difference. I also work on side projects whenever I am less busy with stuff. Some of my side projects are 1. ReBlug - a social platform for bloggers; 2. PublicTrades - An umbrella company under which all my projects are created. \n\nWhile my days are spent immersed in the world of Javascript, React, and Node.js, my evenings and weekends are dedicated to the most rewarding role I have: being a dad to my four amazing kids. When I'm not busy building the next big thing in the digital world, you can find me indulging my love for fishing, enjoying the vibrant tapestry of New York City through people-watching, or simply embracing the ebb and flow of life – sometimes as a social butterfly, other times seeking solace in introspection.\n\nWhether it's crafting a sleek user interface or tackling complex backend challenges, I approach each project with a dedication to clean code, efficient solutions, and a touch of creative flair. I'm always eager to connect with fellow developers and explore new opportunities where I can contribute my skills and passion to impactful projects. Let's build something extraordinary together! \n"
+        let funFact = `
+        1. Chris likes to fish.
+        2. Chris has never caught a single fish.
+        3. Chris has four children (including a set of twin).
+        4. Chris was born on Christmas eve, and does not celebrate Christmas or his birthday.
+        5. Chris believes that Christmas is a man-made holiday.
+        6. Chris stubbornly likes to figure something out before seeking help.
+        `
+        const { message } = await req.json();
+
+        const genAI = new GoogleGenerativeAI(apiKey);
+
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-pro",
+            systemInstruction: `If user input includes the following\n'Who is Chris Bitoye', 'Who is Chris', 'Who is Chris B', 'Whats his experience', 'Which school did he attend' refer to ${aboutMe} for answer. Use the information in ${aboutMe} to provide answers to any input from the user. If there are no information to provide the user, ask if they would like Chris to contact them back with an answer. Please closely follow the following instructions:
+            1. Ask for the user's name, and refer to them by name.
+            2. Be sure to ask if the user wants a follow up response from Chris.
+            3. You are Chris' AI Assistant, do NOT introduce yourself as Chris.
+            4. Be sure to be kind, courteous, professional, and provide support to the user.
+            5. After providing an answer to user's question, always finish with an item from the ${funFact}:
+
+            Example:
+            Fun fact: Chris likes to fish, but he is yet to catch his first fish.
+
+            Here below are more examples of how to respond:
+            "Hi there!  My name is Chris' AI Assistant, nice to meet you.  
+            Chris is a full-stack software developer based in New York City. 
+            He is passionate about transforming innovative ideas into tangible digital experiences.  
+            Would you like for Chris to contact you for more information about your question?"
+            `,
+        });
+
+        const generationConfig = {
+            temperature: 1,
+            topP: 0.95,
+            topK: 64,
+            maxOutputTokens: 8192,
+            responseMimeType: "text/plain",
+        };
+
+        async function run() {
+
+            const chatSession = model.startChat({
+                generationConfig,
+                history: [
+                    {
+                        role: "user",
+                        parts: [
+                            { text: message },
+                        ],
+                    },
+                ],
+            });
+
+            const result = await chatSession.sendMessage(message);
+            console.log(result.response.text());
+
+            return result.response.text();
+        }
+
+        const resultText = await run();
+
+        return NextResponse.json({ success: true, result: resultText });
+    } catch (error) {
+        console.error("Error:", error);
+        return NextResponse.error({ status: 500, message: error.message });
     }
 }
--- components/ChatUI.jsx
+++ components/ChatUI.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 
 const ChatUI = ({ handleClose }) => {
   const [message, setMessage] = useState("");
+  const [chatResponse, setChatResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSend = async (e) => {
-    e.preventDefault();
-    console.log(message);
-    // Send data to the server
+  const handleSend = async () => {
+    setLoading(true);
     try {
       const response = await fetch("/api/sendMessage", {
         method: "POST",
@@ -15,40 +15,55 @@ const ChatUI = ({ handleClose }) => {
         },
         body: JSON.stringify({ message }),
       });
-      const data = await response.json();
-      console.log(data);
-      // Handle response
+
+      if (response.ok) {
+        const data = await response.json();
+        setChatResponse(data.result);
+        setMessage(""); // Clear the input
+      } else {
+        console.error("Failed to send message");
+      }
     } catch (error) {
-      console.error("Error sending message:", error);
+      console.error("Error:", error);
     }
-    handleClose();
+    setLoading(false);
   };
 
   return (
     <div>
-      <form onSubmit={handleSend}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSend();
+        }}
+      >
         <div className="flex-1 space-y-6 overflow-y-auto h-60 rounded-xl bg-slate-200 p-4 text-sm leading-6 text-slate-900 shadow-sm sm:text-base sm:leading-7">
           <div className="flex items-start">
             <img
               className="mr-2 h-8 w-8 rounded-full"
               src="https://dummyimage.com/128x128/363536/ffffff&text=J"
+              alt="User avatar"
             />
             <div className="flex rounded-b-xl rounded-tr-xl bg-slate-50 p-4 sm:max-w-md md:max-w-2xl">
               <p className="text-gray-400">
-                Ask anything you'd like to know about me. This tool will tell
-                you everything there is to know.
+                {message
+                  ? message
+                  : `Ask anything you'd like to know about me. This tool will tell
+                you everything there is to know.`}
               </p>
             </div>
           </div>
           <div className="flex flex-row-reverse items-start">
-            {/* Guest Avatar */}
             <img
               className="ml-2 h-8 w-8 rounded-full"
               src="https://dummyimage.com/128x128/354ea1/ffffff&text=G"
+              alt="Guest avatar"
             />
-            {/* Guest Convo */}
             <div className="flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl">
-              {/* <p>Guest Convo  - Response goes here</p> */}
+              {/* Placeholder for guest conversation */}
+              <p className="text-gray-400">
+                {loading ? "Loading..." : chatResponse}
+              </p>
             </div>
           </div>
         </div>
@@ -56,22 +71,24 @@ const ChatUI = ({ handleClose }) => {
         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <button
             type="button"
-            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
             onClick={handleClose}
           >
             Close
           </button>
           <button
             type="submit"
-            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+            disabled={loading}
           >
-            Send
+            {loading ? "Sending..." : "Send"}
           </button>
           <input
             className="w-56"
             placeholder="Type something here"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            disabled={loading}
           />
         </div>
       </form>
